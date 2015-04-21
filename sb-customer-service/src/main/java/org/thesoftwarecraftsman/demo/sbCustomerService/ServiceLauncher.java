581ac0fb0c72756828e3215da3bf7f8397c7bf2c
Kad DEMBELE <kad@thesoftwarecraftsman.org>
2015-04-21 12:07:17 +0200

    Service 1: Adding a main launcher which will initialize spring context and the embedded server

---
+++ sb-customer-service/src/main/java/org/thesoftwarecraftsman/demo/sbCustomerService/ServiceLauncher.java
@@ -0,0 +1,13 @@
+package org.thesoftwarecraftsman.demo.sbCustomerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLauncher.class, args);
+	}
+
+}
