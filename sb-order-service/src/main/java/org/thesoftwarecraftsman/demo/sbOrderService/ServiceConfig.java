b19ef142a1cc2beb3af46fa80f0183a75cd46812
Kad DEMBELE <kad@thesoftwarecraftsman.org>
2015-04-21 14:56:45 +0200

    Service 3: Adding a restTemplate Bean in ServiceConfig class, because this service should be able to consume other services

--- sb-order-service/src/main/java/org/thesoftwarecraftsman/demo/sbOrderService/ServiceConfig.java
+++ sb-order-service/src/main/java/org/thesoftwarecraftsman/demo/sbOrderService/ServiceConfig.java
@@ -3,7 +3,9 @@ package org.thesoftwarecraftsman.demo.sbOrderService;
 import javax.inject.Named;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ServiceConfig {
@@ -13,5 +15,10 @@ public class ServiceConfig {
 		public JerseyConfig() {
 			this.packages("org.thesoftwarecraftsman.demo.sbOrderService.restAPI");
 		}
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
